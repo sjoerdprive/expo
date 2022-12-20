ef47945265e3ae0d83974960b48e018940ada961
sjoerdprive <sjrdwillemsen@gmail.com>
2022-12-20 11:58:58 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '../scss/style.scss';
+
+export default function DefaultLayout({ children }: any) {
+  return (
+    <html lang="nl">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Expo</title>
+      </head>
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
